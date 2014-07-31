6c4b99ef327e3023b188a491afc3225fc9284b09
Shannon Smith <shannones2013@gmail.com>
2014-07-30 20:40:47 -0700

    add

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-document
-  .getElementsByTagName('body')[0]
-  .appendChild(myMap); 
+var Game = {
+	//is going to be our map
+	display: null,
+
+	//the script to init the game
+	init: function(){
+		this.display = new ROT.Display({ height: 10, width: 20 });
+		document.body.appendChild(this.display.getContainer()) 
+		this._generateMap();
+	}
+}
+
+Game.map = {};
+Game._generateMap = function() {
+    var digger = new ROT.Map.Digger();
+ 
+    var digCallback = function(x, y, value) {
+        if (value) { return; } /* do not store walls */
+ 
+        var key = x+","+y;
+        this.map[key] = "·";
+    }
+    digger.create(digCallback.bind(this));
+    this._drawWholeMap();
+}
+
+Game._drawWholeMap = function() {
+    for (var key in this.map) {
+        var parts = key.split(",");
+        var x = parseInt(parts[0]);
+        var y = parseInt(parts[1]);
+        this.display.draw(x, y, this.map[key]);
+    }
+}
+
+//document
+  //.getElementsByTagName('body')[0]
+  //.appendChild(myMap); 
 
   
